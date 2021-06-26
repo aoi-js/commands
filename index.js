6262b4459c8722671c09ef40de0d7a7fa3cf8500
aoi-js <86505328+aoi-js@users.noreply.github.com>
2021-06-26 10:09:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ const Aoijs = require('aoi.js');
 const bot = new Aoijs.Bot({
 token: "YOUR_TOKEN",
 prefix: ["!", "<@$clientID> "]
+});
+
+bot.onMessage()
+bot.loadCommands('./commands/')
+bot.status({
+text: "NO ONE",
+type: "WATCHING", time: "12"
+})
+bot.status({
+text: "WIND",
+type: "LISTENING", time: "12" //12 Second//
+     //TYPE: LISTENING//
 })
